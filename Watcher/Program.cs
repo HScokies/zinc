5044e878918f5edea35fe6c2aeff7494db7ae1c5
HScokies <hscokies@outlook.com>
2023-06-20 16:56:02 +0500

    Avg: 17,691ms

--- Watcher/Program.cs
+++ Watcher/Program.cs
@@ -6,8 +6,7 @@ internal class Program
     private static void Main(string[] args)
     {
         Appconfig.Init();
-        //OpenMenu();
-        csvDumper();
+        OpenMenu();
     }
     private static void csvDumper()
     {
